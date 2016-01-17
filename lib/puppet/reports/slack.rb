35ce043155952ca37c1b8a5db1473f494696bbf9
Francisco A. Lozano <flozano@gmail.com>
2016-01-17 12:35:49 +0100

    Customization of messages and conditions

--- lib/puppet/reports/slack.rb
+++ lib/puppet/reports/slack.rb
@@ -15,70 +15,91 @@ Puppet::Reports.register_report(:slack) do
     @config["statuses"] ||= "changed,failed"
     statuses = @config["statuses"].split(",")
 
-    # Kernel#` should always run on puppetserver host
-    puppetmaster_hostname = `hostname`.chomp
-    pretxt = "Puppet status: *%s*" % self.status
-    message = <<-FORMAT % [puppetmaster_hostname, self.host, self.environment]
-```
-Puppet Master Host = %s
-Provisioned Host   = %s
-Run Environment    = %s
-```
-    FORMAT
-    color = nil
+    pretext = "#{self.host} *#{self.status}* (#{self.environment})"
 
     if statuses.include?(self.status)
       case self.status
-      when "changed"
-        pretxt = ":congratulations: #{pretxt}"
-        color = 'good'
-      when "failed"
-        pretxt = ":warning: #{pretxt}"
-        color = 'warning'
-      when "unchanged"
-        pretxt = ":zzz: #{pretxt}"
-        color = '#cccccc'
-      else
-        pretxt = ":grey_question: #{pretxt}"
-        color = 'warning'
+        when "changed"
+          pretext = ":balloon: #{pretext}"
+          color = 'good'
+        when "failed"
+          pretext = ":warning: #{pretext}"
+          color = 'warning'
+        when "unchanged"
+          pretext = ":zzz: #{pretext}"
+          color = '#cccccc'
+        else
+          pretext = ":grey_question: #{pretext}"
+          color = 'warning'
       end
 
-      payload = make_payload(pretxt, message, color)
+      payload = make_payload(pretext, message, color)
 
       @config["channels"].each do |channel|
         channel.gsub!(/^\\/, '')
         _payload = payload.merge("channel" => channel)
-        post_to_webhook(URI.parse(@config["webhook"]), _payload)
+        post_to_webhook(URI.parse(@config["webhook"]), channel, _payload)
         Puppet.notice("Notification sent to slack channel: #{channel}")
       end
     end
   end
 
+  def status_of(r)
+    if r.failed then
+      return 'failed'
+    elsif r.skipped then
+      return 'skipped'
+    elsif r.changed then
+      return 'changed'
+    else
+      return 'unchanged'
+    end
+  end
+
   private
-  def make_payload(pretxt, message, color)
+
+  def message
+    resources_message + logs_message
+  end
+
+  def resources_message
+    value = ''
+    self.resource_statuses.values.select { |x| x.changed || x.failed }.
+        group_by { |r| self.status_of(r) }.
+        each { |st, resources|
+      value=value+"*#{st}*:  #{resources.map { |r| "#{r.resource_type}[#{r.title}]" }.join(', ')}\n"
+    }
+    value
+  end
+
+  def logs_message
+    log_text = self.logs.select { |x| self.status == 'failed' ||
+        x.level == :warning || x.level == :err ||
+        x.message =~ /\[(.*) changed to (.*)/
+    }.
+        sort { |a, b| a.time <=> b.time }.
+        map { |x| "#{x.time} - #{x.level} - #{x.source}: #{x.message.gsub("\n", '')}" }.join("\n")
+
+    return "```\n#{log_text.gsub(/^(.{6000,}?).*$/m, '\1\n...')}\n```"
+  end
+
+  def make_payload(pretext, message, color)
     {
-      "username" => (@config["username"] || "puppet"),
-      "icon_url" => (@config["icon_url"] || "https://cloud.githubusercontent.com/assets/91011/6860448/96c424a0-d46b-11e4-9131-31a711893171.png"),
-      "attachments" => [{
-          "pretext" => pretxt,
-          "text"    => message,
-          "mrkdwn_in" => [:text, :pretext],
-          "color"   => color,
-        }],
+        "username" => (@config["username"] || "puppet"),
+        "attachments" => [{
+                              "pretext" => pretext,
+                              "text" => message,
+                              "mrkdwn_in" => [:text, :pretext],
+                              "color" => color,
+                          }],
     }
   end
 
-  def post_to_webhook(uri, payload)
+  def post_to_webhook(uri, channel, payload)
     https = Net::HTTP.new(uri.host, 443)
     https.use_ssl = true
-    r = https.start do |https|
-      https.post(uri.path, payload.to_json)
-    end
-    case r
-    when Net::HTTPSuccess
-      return
-    else
-      Puppet.err("Notification sent faild to slack channel: #{channel}")
-    end
+    result = https.post(uri.path, payload.to_json)
+    Puppet.err("POST returned #{result.code} #{result.msg} (body=#{result.body})") unless result.code == 200
+    result
   end
 end
